2c2bea7bed93afe42c6848d7d579c4982476d35b
Phil Bull <philbull@gmail.com>
2021-06-25 14:33:16 +0100

    Finish tidying-up merging of modules

--- fastbox/__init__.py
+++ fastbox/__init__.py
@@ -1,3 +1,3 @@
 
 from .box import CosmoBox
-from . import beams, box, filters, foregrounds, halos, noise, plot, tracers, point_sources, psm_fgs, lsq_fit
+from . import beams, box, filters, foregrounds, halos, noise, plot, tracers
